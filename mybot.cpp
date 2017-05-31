93d1e71f3dd847e37f6764d07f5c12ace74a895f
Danilo Spinella <danyspin.ds@gmail.com>
2017-05-31 19:37:35 +0200

    Add using statemen

--- mybot.cpp
+++ mybot.cpp
@@ -1,48 +1,57 @@
 // Include the framework
 #include "cppgram/cppgram.hpp"
 
+using std::string;
+
+using cppgram::Polling;
+using cppgram::BasicBot;
+
+using cppgram::types::Message;
+
+using cppgram::commands::MessageCommand;
+
 // Create the bot class
-class MyBot : public cppgram::BasicBot<MyBot> {
+class MyBot : public BasicBot<MyBot> {
 public:
   // Constructor
-  MyBot(std::string &token) : BasicBot(token, "MyBot", this) {}
+  MyBot(string &token) : BasicBot(token, "MyBot", this) {}
 
   // Copy constructor (needed)
   MyBot(const MyBot &b) : BasicBot(b, this) {}
 };
 
 // Function triggered on all messages that don't trigger a command
-void processMessage(MyBot &bot, const cppgram::types::Message &message) {
+void processMessage(MyBot &bot, const Message &message) {
   // Answer with the text sent by the user
   bot.sendMessage("Hello folks!");
 }
 
 // Function called on /start messages
-void startCommand(MyBot &bot, const cppgram::types::Message &message) {
+void startCommand(MyBot &bot, const Message &message) {
   // Answer
-  bot.sendMessage("Hello folks!");
+  bot.sendMessage("This is a start message");
 }
 
 int main() {
   // Declare your bot token
-  std::string token = "token";
+  string token = "token";
   auto bot = MyBot(token);
 
   // Set the pointer to the function triggered each message
   bot.processMessage = &processMessage;
 
   // The command that will trigger the bot
-  std::string name = "start";
+  string name = "start";
 
   // Create the command
-  cppgram::commands::MessageCommand<MyBot> *command =
-      new cppgram::commands::MessageCommand<MyBot>(name, &startCommand);
+  MessageCommand<MyBot> *command =
+      new MessageCommand<MyBot>(name, &startCommand);
 
   // Activate it
   bot.commands().addCommand(command);
 
   // Create the polling with 8 threads
-  auto poll = cppgram::Polling<MyBot>(bot, 8);
+  auto poll = Polling<MyBot>(bot, 8);
 
   // then start it
   poll.run();
